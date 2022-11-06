605348876575a0c09851ce1ebcf082afc84e3588
ccrisc <cesarina.criscione@uzh.ch>
2022-11-06 09:26:49 +0000

    token

--- git_setup_R.R
+++ git_setup_R.R
@@ -1,6 +1,10 @@
 #library(usethis)
 #use_github()
 
+library(gitcreds)
+gitcreds_set()
+#token: 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
+
 #STEPS:
 # 1) Commit
 # 2) push = put code in github (up arrow)
